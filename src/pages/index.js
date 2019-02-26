d9753ad3ab258e36433a1b819cdc0369331af89f
Ujjal Kar <ujjalkar17@gmail.com>
2019-02-27 02:14:57 +0530

    done gallery section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,18 @@ import SEO from "../components/seo";
 import { HomeHeader, Banner, BannerButton } from "../utils";
 import img from "../images/bcg/homeBcg.jpeg";
 import QuickInfo from "../components/homePageComponents/QuickInfo";
+import Gallery from "../components/homePageComponents/Gallery-1";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <HomeHeader img={img}>
-      <Banner title="eatery" subtitle="55 main street - Santa Monica, CA">
+      <Banner title="eatery" subtitle="55 main street, Kol- 700001">
         <BannerButton style={{ margin: "2rem auto" }}>Menu</BannerButton>
       </Banner>
     </HomeHeader>
     <QuickInfo />
+    <Gallery />
   </Layout>
 );
 
