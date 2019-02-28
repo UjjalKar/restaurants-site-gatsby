22fe75a887b842ba6d25375f4ff7b846a05e6312
Ujjal Kar <ujjalkar17@gmail.com>
2019-02-28 12:07:32 +0530

    finished site

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { HomeHeader, Banner, BannerButton } from "../utils";
 import img from "../images/bcg/homeBcg.jpeg";
 import QuickInfo from "../components/homePageComponents/QuickInfo";
 import Gallery from "../components/homePageComponents/Gallery-1";
+import Menu from "../components/homePageComponents/Menu";
 
 const IndexPage = () => (
   <Layout>
@@ -17,6 +18,7 @@ const IndexPage = () => (
     </HomeHeader>
     <QuickInfo />
     <Gallery />
+    <Menu />
   </Layout>
 );
 
